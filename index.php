82f7ae96290f350f6ae083116e3f973b7bf85ecd
CKORDA <144379496+CKORDA@users.noreply.github.com>
2023-11-15 19:30:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,39 @@ $teamMembers = [
     [
         "name" => "Cheyenne Korda",
         "role" => "Cyber Security Analyst",
-        "link" => "http://localhost/ase230/02/CHEYENNE_KORDA.php",
+        "link" => "detail.php?index=0",
         "image" => "assets/images/profileCK.jpg"
     ],
     [
         "name" => "Ramatoulaye signate",
         "role" => "software developer",
-        "link" => "http://localhost/ase230/02/Ramatoulaye's.php#",
+        "link" => "detail.php?index=1",
         "image" => "assets/images/profile.jpg"
     ]
 ];
+
+function displayMember($member, $index){
+    echo '<header class="resume-header mt-4 pt-4 pt-md-0">';
+        echo '<div class="row">';
+            echo '<div class="col-block col-md-auto resume-picture-holder text-center text-md-start">';
+                echo '<img class="picture" src="' . $member['image'] .'" alt="">';
+            echo '</div><!--//col-->';
+            echo '<div class="col">';
+                echo '<div class="row p-4 justify-content-center justify-content-md-between">';
+                    echo '<div class="primary-info col-auto">';
+                        echo '<h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">' . $member['name'] . '</h1>';
+                        echo '<div class="title mb-3">' . $member['role'] .'</div>';
+                        echo '<a href="' . $member['link'] .'" class="btn btn-secondary">See full profile</a>';
+                    echo '</div><!--//primary-info-->';
+                    echo '<div class="secondary-info col-auto mt-2">';
+                    echo '</div><!--//secondary-info-->';
+                echo '</div><!--//row-->';
+
+            echo '</div><!--//col-->';
+        echo '</div><!--//row-->';
+    echo '</header>';
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -43,141 +66,20 @@ $teamMembers = [
 <article class="resume-wrapper text-center position-relative">
     <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
         <h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
-
-        <?php foreach ($teamMembers as $member) { ?>
-            <header class="resume-header mt-4 pt-4 pt-md-0">
-                <div class="row">
-                    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-                        <img class="picture" src="<?php echo $member['image']; ?>" alt="">
-                    </div><!--//col-->
-                    <div class="col">
-                        <div class="row p-4 justify-content-center justify-content-md-between">
-                            <div class="primary-info col-auto">
-                                <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?php echo $member['name']; ?></h1>
-                                <div class="title mb-3"><?php echo $member['role']; ?></div>
-                                <a href="<?php echo $member['link']; ?>" class="btn btn-secondary">See full profile</a>
-                            </div><!--//primary-info-->
-                            <div class="secondary-info col-auto mt-2">
-                            </div><!--//secondary-info-->
-                        </div><!--//row-->
-
-                    </div><!--//col-->
-                </div><!--//row-->
-            </header>
-        <?php } ?>
+        <?php foreach($teamMembers as $index => $member){
+            displayMember($member, $index);
+        }
+        ?>
 
     </div>
 </article>
 
 <footer class="footer text-center pt-2 pb-5">
     <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-    <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your names</small>
+    <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by cheyenne and Ramatoulaye</small>
 </footer>
 
-</body>
-</html>
 
-<?php
-// INSERT DATA HERE.
-
-?>
-<!DOCTYPE html>
-<html lang="en"> 
-<head>
-    <title>Our amazing team</title>
-    
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Your name's resume">
-    <meta name="author" content="Your name">    
-    <link rel="shortcut icon" href="favicon.ico"> 
-    
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
-    
-    <!-- FontAwesome JS-->
-	<script defer src="assets/fontawesome/js/all.min.js"></script>
-       
-    <!-- Theme CSS -->  
-    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
-
-
-</head> 
-
-<body>
-    <article class="resume-wrapper text-center position-relative">
-	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
-			<h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
-		    <header class="resume-header pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 1 name</h1>
-							    <div class="title mb-3">Member 1 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_1_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
-		    <header class="resume-header mt-4 pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 2 name</h1>
-							    <div class="title mb-3">Member 2 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_2_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
-		    <header class="resume-header mt-4 pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 3 name</h1>
-							    <div class="title mb-3">Member 3 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_3_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
-	    </div>
-    </article> 
-
-    
-    <footer class="footer text-center pt-2 pb-5">
-	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your names</small>
-    </footer>
 
     
 
